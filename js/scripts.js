b375d57afd107edffd2bc972326c07b2617e8fff
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-04 09:07:25 -0700

    Add constructor function to create a new account that passes the first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 //Business Logic
 
-function Account  {
-  
+function Account(accountNumber, name, balance, type)  {
+  this.AccountNumber = accountNumber;
+  this.Name = name;
+  this.Balance = balance;
+  this.Type = type;
+  this.History = "";
 }
 
 
